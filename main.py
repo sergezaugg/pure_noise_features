c16b51dd1dcee69af7a37a73bdab899000c75c75
Serge Zaugg <serge.zaugg@gmail.com>
2025-03-01 09:00:06 +0100

    included logistic regression into main.py

--- main.py
+++ main.py
@@ -47,29 +47,37 @@ scenarios_di = {
 figs_li = plot_scenarios(scenarios_di, random_seed)
 
 # Evaluate the scenarios (QUICK)
-# nb_noisy_features = [0, 5, 25, 50, 100, 500]
-# nb_trees = 10
+nb_noisy_features = [0, 5, 25, 50, 100, 500]
+nb_trees = 10
+
 # Evaluate the scenarios (FULL)
-nb_noisy_features = [0, 1, 5, 10, 25, 50, 100, 500, 1000]
-nb_trees = 50
+# nb_noisy_features = [0, 1, 5, 10, 25, 50, 100, 500, 1000]
+# nb_trees = 50
+
 resu01 = evaluate_scenarios_rfo(rfo_max_features =  1, sce = scenarios_di, nb_noisy_features = nb_noisy_features,  ntrees = nb_trees, seed = random_seed)
 resu02 = evaluate_scenarios_rfo(rfo_max_features = 10, sce = scenarios_di, nb_noisy_features = nb_noisy_features,  ntrees = nb_trees, seed = random_seed)
 resu03 = evaluate_scenarios_rfo(rfo_max_features = 25, sce = scenarios_di, nb_noisy_features = nb_noisy_features,  ntrees = nb_trees, seed = random_seed)
-resu04 = evaluate_scenarios_logit(sce = scenarios_di, nb_noisy_features = nb_noisy_features, logit_c_param = 10.0, seed = random_seed)
+resu04 = evaluate_scenarios_logit(sce = scenarios_di, nb_noisy_features = nb_noisy_features, logit_c_param =  0.1, seed = random_seed)
+resu05 = evaluate_scenarios_logit(sce = scenarios_di, nb_noisy_features = nb_noisy_features, logit_c_param =  1.0, seed = random_seed)
+resu06 = evaluate_scenarios_logit(sce = scenarios_di, nb_noisy_features = nb_noisy_features, logit_c_param = 10.0, seed = random_seed)
 
 # Prepare results figures 
 fig01 = plot_performance_vs_n_features(resu01)
 fig02 = plot_performance_vs_n_features(resu02)
 fig03 = plot_performance_vs_n_features(resu03)
 fig04 = plot_performance_vs_n_features(resu04)
+fig05 = plot_performance_vs_n_features(resu05)
+fig06 = plot_performance_vs_n_features(resu06)
 
 # Save all figures later re-import 
 for i,f in enumerate(figs_li):
-    f.write_image(os.path.join(path_save_figures, 'scenario' + str(i) + '.png' ) )  #'saved_figures/scenario' + str(i) + '.png') 
+    f.write_image(os.path.join(path_save_figures, 'scenario' + str(i) + '.png' ) )  
 fig01.write_image(os.path.join(path_save_figures, "results_01.png"))
 fig02.write_image(os.path.join(path_save_figures, "results_02.png"))
 fig03.write_image(os.path.join(path_save_figures, "results_03.png"))
 fig04.write_image(os.path.join(path_save_figures, "results_04.png"))
+fig05.write_image(os.path.join(path_save_figures, "results_05.png"))
+fig06.write_image(os.path.join(path_save_figures, "results_06.png"))
 
 # Render all figures directly in this session
 if False:
